f1c558b9bf343fa515e6b7fae7be3941ec416b1a
FTC-21605 <ftc21605@gmail.com>
2024-10-23 19:51:09 -0400

    gradle update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ArmTest.java
@@ -29,10 +29,10 @@
 
 package org.firstinspires.ftc.teamcode.test;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+
 import org.firstinspires.ftc.teamcode.OurTeleOpBase;
 
 /*
@@ -49,18 +49,19 @@ import org.firstinspires.ftc.teamcode.OurTeleOpBase;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
-@TeleOp(name = "Test: Scan Servo", group = "Test")
+@TeleOp(name = "Test: Arm Test", group = "Test")
 //@Disabled
-public class ConceptScanServo extends OurTeleOpBase {
+public class ArmTest extends OurTeleOpBase {
 
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
     static final int    CYCLE_MS    =   50;     // period of each cycle
     static final double MAX_POS     =  0.9;     // Maximum rotational position
     static final double MIN_POS     =  0.0;     // Minimum rotational position
 
+    DcMotor motor;
     // Define class members
-    Servo   servo1;
-    Servo   servo2;
+    Servo   grabber_servo;
+    Servo   rotator_servo;
     double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
     boolean rampUp = true;
 
@@ -68,10 +69,11 @@ public class ConceptScanServo extends OurTeleOpBase {
     @Override
     public void runOpMode() {
 
+       motor = hardwareMap.get(DcMotor.class, "slide0");
         // Connect to servo (Assume Robot Left Hand)
         // Change the text in quotes to match any servo name on your robot.
-        servo1 = hardwareMap.get(Servo.class, "grabber0");
-        servo2 = hardwareMap.get(Servo.class, "rotator1");
+        grabber_servo = hardwareMap.get(Servo.class, "grabber0");
+        rotator_servo = hardwareMap.get(Servo.class, "rotator1");
 
         // Wait for the start button
         telemetry.addData(">", "Press Start to scan Servo." );
@@ -99,17 +101,40 @@ public class ConceptScanServo extends OurTeleOpBase {
                     rampUp = !rampUp;  // Switch ramp direction
                 }
             }
-
-            // Display the current value
+	    if (gamepad1.y)
+		{
+		    motor.setPower(-1.);
+		}
+	    if (gamepad1.a)
+		{
+		    motor.setPower(-0.8);
+		}
+	    if (gamepad1.x)
+		{
+		    motor.setPower(1.);
+		}
+	    if (!gamepad1.y && !gamepad1.x && !gamepad1.a)
+ 		{
+		    motor.setPower(0);
+		}
+	    if (gamepad1.a)
+		{
+            grabber_servo.setPosition(MAX_POS);
+		}
+	    if (gamepad1.b)
+		{
+            grabber_servo.setPosition(MIN_POS);
+		}
+	    // Display the current value
             telemetry.addData("Servo Position", "%5.2f", position);
+            telemetry.addData(">", "Press Stop to end test." );
             telemetry.addData(">", "Press Stop to end test." );
             telemetry.update();
 
             // Set the servo to the new position and pause;
-            servo1.setPosition(position);
-            servo2.setPosition(position);
-            sleep(CYCLE_MS);
-            idle();
+	    //            rotator_servo.setPosition(position);
+	    //            sleep(CYCLE_MS);
+            //idle();
         }
 
         // Signal done;
